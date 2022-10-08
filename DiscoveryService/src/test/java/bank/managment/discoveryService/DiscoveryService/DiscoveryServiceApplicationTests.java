c94d52f79ad3a410794aa7847f658195b8a98fba
Mohamed ZITOUNI <zitouni.mohamed.email@gmail.com>
2022-10-08 13:44:02 +0200

    Video 21 & 22: dockeriser microservices

--- DiscoveryService/src/test/java/bank/managment/discoveryService/DiscoveryService/DiscoveryServiceApplicationTests.java
+++ DiscoveryService/src/test/java/bank/managment/discoveryService/DiscoveryService/DiscoveryServiceApplicationTests.java
@@ -1,4 +1,4 @@
-package bank.managment.discoveryService.DiscoveryService;
+/*package bank.managment.discoveryService.DiscoveryService;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -11,3 +11,4 @@ class DiscoveryServiceApplicationTests {
 	}
 
 }
+*/
